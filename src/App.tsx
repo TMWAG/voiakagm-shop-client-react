8b4e69c4727d3a1983c07c2e10733646713fa728
TMWAG <semyon.valyaev@gmail.com>
2023-05-27 21:41:20 +0300

    добавлены маршруты-заглушки

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled  from 'styled-components';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Pages/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Catalogue from './components/Pages/Catalogue/Catalogue';
+import Sales from './components/Pages/Sales/Sales';
+import Favorites from './components/Pages/Favorites/Favorites';
+import User from './components/Pages/User/User';
+import Cart from './components/Pages/Cart/Cart';
+import Recommendations from './components/Pages/Recommendations/Recommendations';
+
+const Wrapper = styled.div`
+
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Navbar/>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home/>}
+        />
+        <Route
+          path='/catalogue'
+          element={<Catalogue/>}
+        />
+        <Route
+          path='/sales'
+          element={<Sales/>}
+        />
+        <Route
+          path='/favorites'
+          element={<Favorites/>}
+        />
+        <Route
+          path='/user'
+          element={<User/>}
+        />
+        <Route
+          path='/cart'
+          element={<Cart/>}
+        />
+        <Route
+          path='/recommendations'
+          element={<Recommendations/>}
+        />
+      </Routes>
+    </Wrapper>
   );
 }
 
