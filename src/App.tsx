df6a63bb4fb61d0af84e65af988f7e03a9d0f8c6
TMWAG <semyon.valyaev@gmail.com>
2023-05-30 20:33:19 +0300

    рефакторинг

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,21 @@ import React from 'react';
 import './App.css';
 import styled from 'styled-components';
 import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
 import { Route, Routes } from 'react-router-dom';
-import Catalogue from './components/Catalogue/Catalogue';
-import Sales from './components/Sales/Sales';
-import User from './components/User/User';
-import Cart from './components/Cart/Cart';
-import Recommendations from './components/Recommendations/Recommendations';
-import Guarantees from './components/Guarantees/Guarantees';
-import DeliveryInfo from './components/DeliveryInfo/DeliveryInfo';
+import { routerRoutes } from './common/routes/routerRoutes';
 
-const Wrapper = styled.div``;
+const StyledWrapper = styled.div``;
 
 function App(): JSX.Element {
   return (
-    <Wrapper>
+    <StyledWrapper>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/delivery_info" element={<DeliveryInfo />} />
-        <Route path="/guarantees" element={<Guarantees />} />
-        <Route path="/catalogue" element={<Catalogue />} />
-        <Route path="/sales" element={<Sales />} />
-        <Route path="/user" element={<User />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/recommendations" element={<Recommendations />} />
+        {routerRoutes.map((r) => (
+          <Route {...r} key={r.path.concat('Router')} />
+        ))}
       </Routes>
-    </Wrapper>
+    </StyledWrapper>
   );
 }
 
