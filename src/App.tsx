080b91fea2b89a6acafe406efb4f47192e9eb808
TMWAG <semyon.valyaev@gmail.com>
2023-05-30 23:51:23 +0300

    убран StyledWrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import './App.css';
-import styled from 'styled-components';
 import Navbar from './components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import { routerRoutes } from './common/routes/routerRoutes';
 
-const StyledWrapper = styled.div``;
-
 function App(): JSX.Element {
   return (
-    <StyledWrapper>
+    <div>
       <Navbar />
       <Routes>
         {routerRoutes.map((r) => (
           <Route {...r} key={r.path.concat('Router')} />
         ))}
       </Routes>
-    </StyledWrapper>
+    </div>
   );
 }
 
