bc8e6729c77a86d25abb45eb71f47e097dc4aa46
TMWAG <semyon.valyaev@gmail.com>
2023-05-31 22:07:51 +0300

    новый компонент

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import styles from './Home.module.css';
 import TopPromoted from './Promoted/TopPromoted';
 import Categories from './Categories/Categories';
+import InfoBlock from './InfoBlock/InfoBlock';
 
 const Home = (): JSX.Element => {
   return (
     <main className={styles.home}>
-      home
       <TopPromoted />
+      <InfoBlock />
       <Categories />
     </main>
   );
