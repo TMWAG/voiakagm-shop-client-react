4f32cc9d42bd1186d27be23ff69c75dfade43a6e
TMWAG <semyon.valyaev@gmail.com>
2023-05-31 14:46:31 +0300

    удалён стилизованный компонент

--- src/components/Home/StyledHome.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledHome = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: pink;
-  margin: 2vh 0;
-`;
