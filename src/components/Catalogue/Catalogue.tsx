3fdd5bb767ee03004e7e5789b3f1ffe13787fb77
TMWAG <semyon.valyaev@gmail.com>
2023-06-01 23:46:22 +0300

    useEffect заменён на useCategoryProducts, проверка на тип id категории

--- src/components/Catalogue/Catalogue.tsx
+++ src/components/Catalogue/Catalogue.tsx
@@ -1,46 +1,34 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { setProducts } from '../../store/slices/productsSlice';
 import { type RootState } from '../../store/store';
 import styles from './Catalogue.module.css';
 import CatalogueItem from './CatalogueItem/CatalogueItem';
+import { useCategoryProducts } from '../../hooks/useCategoryProducts';
 
 const Catalogue = (): JSX.Element => {
-  const dispatch = useDispatch();
   const products = useSelector((state: RootState) => state.products.products);
   const { categoryId } = useParams();
-  useEffect(() => {
-    axios
-      .get(`http://localhost:5000/api/product/all`, {
-        params: {
-          categoryId,
-        },
-      })
-      .then(({ data }) => {
-        dispatch(setProducts(data));
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, [categoryId, products]);
-  return (
-    <div className={styles.catalogue}>
-      Catalogue
-      {products.map((p) => (
-        <CatalogueItem
-          key={p.id}
-          id={p.id}
-          name={p.name}
-          pictures={p.pictures}
-          price={p.price}
-          discount={p.discount}
-          used={p.used}
-        />
-      ))}
-    </div>
-  );
+  if (!Number.isNaN(categoryId)) {
+    useCategoryProducts(Number(categoryId));
+    return (
+      <div className={styles.catalogue}>
+        {products.map((p) => (
+          <CatalogueItem
+            key={p.id}
+            id={p.id}
+            name={p.name}
+            pictures={p.pictures}
+            price={p.price}
+            discount={p.discount}
+            used={p.used}
+          />
+        ))}
+      </div>
+    );
+  } else {
+    return <div>Не категория не указана</div>;
+  }
 };
 
 export default Catalogue;
