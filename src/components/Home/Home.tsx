aa18610938059f97e55ef45382fe7053b42d57b9
TMWAG <semyon.valyaev@gmail.com>
2023-06-01 23:10:58 +0300

    компонент TopPromoted закомментирован до реализации на сервере

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './Home.module.css';
-import TopPromoted from './Promoted/TopPromoted';
+// import TopPromoted from './Promoted/TopPromoted';
 import Categories from './Categories/Categories';
 import InfoBlock from './InfoBlock/InfoBlock';
 
 const Home = (): JSX.Element => {
   return (
     <main className={styles.home}>
-      <TopPromoted />
+      {/* <TopPromoted /> */}
       <InfoBlock />
       <Categories />
     </main>
